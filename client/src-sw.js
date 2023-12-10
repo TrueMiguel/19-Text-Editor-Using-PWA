29b4f7ff0cdda82efcd955a45d05f0ff394637fd
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-12-10 14:46:35 -0500

    got the install button to work but now for some reason it is not launching, even after clearing the cache. Got the database to work properly now.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,8 +1,10 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+// adding StaleWhileRevalidate
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { ExpirationPlugin } = require('workbox-expiration');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -28,7 +30,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => request.destination === 'script' || request.destination === 'style', 
-  new CacheFirst({
+  new StaleWhileRevalidate({
     cachename: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,18 +18,18 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(content);
+  const request = store.put({ id:1, value: content });
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log('Data saved to the database', result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('Result.value', result);
   return result;
